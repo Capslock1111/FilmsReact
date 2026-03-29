b3723d4c672a517e097a9f190eddf9232756b5a3
Capslock1111 <lockcaps3228@gmail.com>
2026-03-29 10:17:17 +0300

    обновил конфиг

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 export default defineConfig({
   plugins: [react()],
+  // ✅ Если есть base, убедитесь что он правильный
+  base: '/FilmsReact',
 })
